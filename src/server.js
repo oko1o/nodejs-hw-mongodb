2011f48e512871c57fb62d342e7cbb17ac12bd1c
oko1o <okolomanov@gmail.com>
2024-08-31 10:06:13 -0400

    nodejs hw2 v1.1

--- src/server.js
+++ src/server.js
@@ -36,17 +36,17 @@ export const setupServer = () => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
 
+    if (!contact) {
+      return res.status(404).json({
+        message: `Contact with ID ${contactId} not found`,
+      });
+    }
+
     res.status(200).json({
       status: 200,
       message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
-
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
-      });
-    }
   });
 
   app.use((req, res) => {
